226b404d36496ec6f88522938a9282a62cebcf4e
joaoRocha47196 <joaocrocha2001@gmail.com>
2021-03-16 15:05:48 +0000

    FirstCommit

---
+++ Point.cs
@@ -0,0 +1,15 @@
+public struct Point{
+
+    public double x;
+    public double y;
+    
+    public Point (double x1, double y1){
+        x = x1;
+        y = y1;
+    }
+
+    public void getModule(){
+        return 1; //Math.sqrt()
+    }
+
+}
